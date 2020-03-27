59e20c708a8e049385c6ca9f36ca2fe9559962d4
dhkwong <daryl.h.kwong@gmail.com>
2020-03-27 14:13:15 -0700

    researching still, but just setting up components and game structure

---
+++ public/src/app/spymaster/spymaster.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpymasterComponent } from './spymaster.component';
+
+describe('SpymasterComponent', () => {
+  let component: SpymasterComponent;
+  let fixture: ComponentFixture<SpymasterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SpymasterComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SpymasterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ public/src/app/spymaster/spymaster.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spymaster',
+  templateUrl: './spymaster.component.html',
+  styleUrls: ['./spymaster.component.css']
+})
+export class SpymasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
